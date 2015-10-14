e960db44fc1963ccfa94cbbee9fc4a148aef8016
xuzhenglun <xuzhenglun@gmail.com>
2015-10-14 22:26:53 +0800

    enhance baidu map

--- server.go
+++ server.go
@@ -140,6 +140,16 @@ const Template string = `
 	map.centerAndZoom(point,16);
 	var myGeo = new BMap.Geocoder();
 	map.addOverlay(new BMap.Marker(point));
+	var mapType1 = new BMap.MapTypeControl({mapTypes: [BMAP_NORMAL_MAP,BMAP_HYBRID_MAP]});
+	var mapType2 = new BMap.MapTypeControl({anchor: BMAP_ANCHOR_TOP_LEFT});
+	var overView = new BMap.OverviewMapControl();
+	var overViewOpen = new BMap.OverviewMapControl({isOpen:true, anchor: BMAP_ANCHOR_BOTTOM_RIGHT});
+	//添加地图类型和缩略图
+	map.addControl(mapType1);          //2D图，卫星图
+	map.addControl(mapType2);          //左上角，默认地图控件
+	map.setCurrentCity("北京");        //由于有3D图，需要设置城市哦
+	map.addControl(overView);          //添加默认缩略地图控件
+	map.addControl(overViewOpen);      //右下角，打开
 
 </script>
 `
