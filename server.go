07b6711193ecfcdccdf9bd4814dbc03e6994bac5
xuzhenglun <xuzhenglun@gmail.com>
2015-10-14 22:05:13 +0800

    Add baidu Map support

--- server.go
+++ server.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/xuzhenglun/Project/GpsHandle"
 	"github.com/xuzhenglun/project/GPS_Recv"
+	"github.com/xuzhenglun/project/GpsHandle"
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 var data GpsHandle.GPRMC
@@ -57,6 +58,24 @@ func showInGoogleMap(w http.ResponseWriter, r *http.Request) {
 }
 
 func showInBaiduMap(w http.ResponseWriter, r *http.Request) {
+	LatitdeH := int(data.Latitde / 100)
+	LatitdeM := int(data.Latitde - float64(LatitdeH)*100)
+	LatitdeS := (data.Latitde - float64(100*LatitdeH+LatitdeM)) * 60
+
+	LongitudeH := int(data.Longitude / 100)
+	LongitudeM := int(data.Longitude - float64(LongitudeH)*100)
+	LongitudeS := (data.Longitude - float64(100*LongitudeH+LongitudeM)) * 60
+
+	Latitde := float64(LatitdeH) + float64(LatitdeM)/60 + LatitdeS/3600
+	Longitude := float64(LongitudeH) + float64(LongitudeM)/60 + LongitudeS/3600
+	x, y, err := GpsToMars(Longitude, Latitde)
+	if err != 0 {
+		fmt.Printf("Error %d: Fail to Go to Mars.\n", err)
+	}
+	template := Template
+	template = strings.Replace(template, "MAGIC_X", strconv.FormatFloat(x, 'g', 20, 64), -1)
+	template = strings.Replace(template, "MAGIC_Y", strconv.FormatFloat(y, 'g', 20, 64), -1)
+	fmt.Fprintf(w, "%s", template)
 }
 
 func returnApi(w http.ResponseWriter, r *http.Request) {
@@ -77,11 +96,11 @@ type Gps struct {
 }
 
 type GpsMars struct {
-	Status bool
-	Result Gps
+	Status int
+	Result []Gps
 }
 
-func GpsToMars(x, y float64) (float64, float64, bool) {
+func GpsToMars(x, y float64) (float64, float64, int) {
 	var mars GpsMars
 	client := &http.Client{}
 	reqest, _ := http.NewRequest("GET", "http://api.map.baidu.com/geoconv/v1/?coords="+strconv.FormatFloat(x, 'g', 10, 64)+","+strconv.FormatFloat(y, 'g', 10, 64)+"&from=1&to=5&ak=F37d12e0fc7f53d91bbe11819a0b8626", nil)
@@ -92,7 +111,35 @@ func GpsToMars(x, y float64) (float64, float64, bool) {
 		err := json.Unmarshal(b, &mars)
 		if err == nil {
 			fmt.Println(mars)
+		} else {
+			fmt.Println(err)
 		}
 	}
-	return mars.Result.X, mars.Result.Y, mars.Status
+	return mars.Result[0].X, mars.Result[0].Y, mars.Status
 }
+
+const Template string = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<style type="text/css">
+		body, html,#allmap {width: 100%;height: 100%;overflow: hidden;margin:0;font-family:"微软雅黑";}
+	</style>
+	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=F37d12e0fc7f53d91bbe11819a0b8626"></script>
+	<title>地址解析</title>
+</head>
+<body>
+	<div id="allmap"></div>
+</body>
+</html>
+<script type="text/javascript">
+	var map = new BMap.Map("allmap");
+	var point = new BMap.Point(MAGIC_X,MAGIC_Y);
+	map.centerAndZoom(point,16);
+	var myGeo = new BMap.Geocoder();
+	map.addOverlay(new BMap.Marker(point));
+
+</script>
+`
